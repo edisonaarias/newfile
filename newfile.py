a9310efa6bf31507a59e56340f983505f9ccbec5
unknown <edisonalexanderarias@gmail.com>
2023-02-12 18:55:04 -0800

    adding pet conditional

---
+++ newfile.py
@@ -0,0 +1,10 @@
+pet_name = "lambeau"
+
+if(pet_name == "lambeau"):
+    print('Best pet name ever!')
+
+elif(pet_name == "Denali"):
+    print("she is a yellow lab")
+else:
+    print("That is not the name of one of my pets")
+    
