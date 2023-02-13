3c5d84bceddf23e7ed811190ad8237f988bcc1e9
unknown <edisonalexanderarias@gmail.com>
2023-02-12 19:09:15 -0800

    Added nested if

--- newfile.py
+++ newfile.py
@@ -1,10 +1,13 @@
-pet_name = "lambeau"
+pet_name = "Denali"
+hair_color = "black"
 
 if(pet_name == "lambeau"):
     print('Best pet name ever!')
-
+    if (hair_color == "black" or hair_color == "brown"):
+        print("Beautiful looking dog")
 elif(pet_name == "Denali"):
     print("she is a yellow lab")
+elif(pet_name == "Rocky"):
+    print("Super cool name!")
 else:
     print("That is not the name of one of my pets")
-    
