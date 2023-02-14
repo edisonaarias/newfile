7a33880733f6c909894ccc39ac13bea6452b60df
unknown <edisonalexanderarias@gmail.com>
2023-02-13 19:03:07 -0800

    Accessed the second index in the list

--- listfile.py
+++ listfile.py
@@ -1,7 +1,11 @@
-#creating a list
+# creating a list
 first_names = ['Mina','Momo','Sana']
 print(first_names)
 
-#adding a value
+# adding a value
 first_names.append('Dahyun')
 print(first_names)
+
+# accessing a value
+element_at_second_index = first_names [2]
+print(element_at_second_index)
