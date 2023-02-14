745dd35c6de2dad6412ab75cc0ed8b24c6f9208d
unknown <edisonalexanderarias@gmail.com>
2023-02-13 18:59:51 -0800

    appended a value

--- listfile.py
+++ listfile.py
@@ -1,4 +1,7 @@
 #creating a list
-frist_names = ['Mina','Momo','Sana']
-print(frist_names)
+first_names = ['Mina','Momo','Sana']
+print(first_names)
 
+#adding a value
+first_names.append('Dahyun')
+print(first_names)
