8ac9bf26e17a0bd0c5fc209e0b7e3754ed39275a
unknown <edisonalexanderarias@gmail.com>
2023-03-01 22:46:51 -0800

    Added inheritance to the file

--- newfile.py
+++ newfile.py
@@ -48,3 +48,16 @@ def add_names(first_name, last_name):
 
 result_from_second_function = add_names("edison", "arias")
 print(result_from_second_function)
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
