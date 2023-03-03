bb02aa7e43833652d83a09161474657b045bb8f9
unknown <edisonalexanderarias@gmail.com>
2023-03-02 22:55:49 -0800

    build an even number checker program

--- newfile.py
+++ newfile.py
@@ -1,16 +1,16 @@
-pet_name = "Denali"
-hair_color = "black"
+# pet_name = "Denali"
+# hair_color = "black"
 
-if(pet_name == "lambeau"):
-    print('Best pet name ever!')
-    if (hair_color == "black" or hair_color == "brown"):
-        print("Beautiful looking dog")
-elif(pet_name == "Denali"):
-    print("she is a yellow lab")
-elif(pet_name == "Rocky"):
-    print("Super cool name!")
-else:
-    print("That is not the name of one of my pets")
+# if(pet_name == "lambeau"):
+#     print('Best pet name ever!')
+#     if (hair_color == "black" or hair_color == "brown"):
+#         print("Beautiful looking dog")
+# elif(pet_name == "Denali"):
+#     print("she is a yellow lab")
+# elif(pet_name == "Rocky"):
+#     print("Super cool name!")
+# else:
+#     print("That is not the name of one of my pets")
 
 
 # while loop 
@@ -26,41 +26,49 @@ else:
         #age += 1
 
 # for loop 
-best_football_team = "packers"
+# best_football_team = "packers"
 
 #print(best_football_team[2])
 
-for character in best_football_team:
-    if(character == 'c'):
-        continue
-    print(character)
+# for character in best_football_team:
+#     if(character == 'c'):
+#         continue
+#     print(character)
 
-def add_two_numbers(number_one, number_two):
-    result = number_one + number_two
-    return result
+# def add_two_numbers(number_one, number_two):
+#     result = number_one + number_two
+#     return result
 
-result_from_function = add_two_numbers(300, 200)
-print(result_from_function)
+# result_from_function = add_two_numbers(300, 200)
+# print(result_from_function)
 
-def add_names(first_name, last_name):
-    result = first_name + last_name
-    return result 
+# def add_names(first_name, last_name):
+#     result = first_name + last_name
+#     return result 
 
-result_from_second_function = add_names("edison", "arias")
-print(result_from_second_function)
+# result_from_second_function = add_names("edison", "arias")
+# print(result_from_second_function)
 
-class Person:
-  def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
+# class Person:
+#   def __init__(self, fname, lname):
+#     self.firstname = fname
+#     self.lastname = lname
 
-  def printname(self):
-    print(self.firstname, self.lastname)
+#   def printname(self):
+#     print(self.firstname, self.lastname)
 
-#Use the Person class to create an object, and then execute the printname method:
+# #Use the Person class to create an object, and then execute the printname method:
 
-x = Person("John", "Doe")
-x.printname()
+# x = Person("John", "Doe")
+# x.printname()
 
-thislist = ["apple", "banana", "cherry"]
-print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+
+num = int(input('Enter a number:'))
+
+if num%2 == 0:
+    print('Even number')
+else:
+    print('Odd number')
+    
